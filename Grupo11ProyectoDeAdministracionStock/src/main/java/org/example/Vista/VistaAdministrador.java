ed1f75c383afe936efdc04ceccc8594bb830b9a1
HernandezVicente <v.hernandez09@ufromail.cl>
2025-10-29 23:11:36 -0300

    Delete Grupo11ProyectoDeAdministracionStock/src/main/java/org/example/Vista directory

--- Grupo11ProyectoDeAdministracionStock/src/main/java/org/example/Vista/VistaAdministrador.java
+++
@@ -1,7 +0,0 @@
-package org.example.Vista;
-
-public class VistaAdministrador {
-    /*esta clase es solo para las vista del administrador
-    por lo que es la interface la cual se usar JSP
-     */
-}
