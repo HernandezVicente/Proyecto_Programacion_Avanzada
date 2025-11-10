7dfe0643740c8e5e9972f3f4a6943d45d74e091a
volcano56v <hayatolegendario@gmail.com>
2025-11-10 09:58:01 -0300

    solo un comentario
    fue para ver cómo usar el GitHub de escritorio

--- Grupo11ProyectoDeAdministracionStock/src/main/java/org/example/logica/BodegaServlet.java
+++ Grupo11ProyectoDeAdministracionStock/src/main/java/org/example/logica/BodegaServlet.java
@@ -12,6 +12,8 @@ import java.util.List;
 public class BodegaServlet extends HttpServlet {
     private final LogicaBodega logica = new LogicaBodega();
 
+    //este es un comentario en java
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
